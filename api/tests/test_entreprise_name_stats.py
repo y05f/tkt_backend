71019904aeaf21bfdabd23f5fd79fbeb5ef2adab
y05f <ysf.safi@gmail.com>
2022-12-12 21:35:28 +0100

    tests edited

--- api/tests/test_entreprise_name_stats.py
+++ api/tests/test_entreprise_name_stats.py
@@ -18,7 +18,7 @@ class EntrepriseNameStatsTest(APITestCase):
 
     def test_get_name_list(self):
         """
-        Ensure we can get a all names of entreprises.
+        Ensure we can get all names of entreprises.
         """
         url = reverse("list-entreprise-name")
         response = self.client.get(url, format="None")
@@ -30,5 +30,66 @@ class EntrepriseNameStatsTest(APITestCase):
                 {"name": "McKenzie LLC", "count": 2},
                 {"name": "Towne-Dach", "count": 1},
                 {"name": "Reinger Inc", "count": 1},
+            ],
+        )
+
+    def test_get_name_details(self):
+        """
+        Ensure we can get all details of entreprise by name.
+        """
+        url = reverse("entreprise-name-details", kwargs={"name": "Reinger Inc"})
+        response = self.client.get(url, format="None")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data,
+            [
+                {
+                    "name": "Reinger Inc",
+                    "sector": "Services",
+                    "siren": "135694027",
+                    "results": [
+                        {
+                            "ca": 2077357,
+                            "margin": 497351,
+                            "ebitda": 65952,
+                            "loss": 858474,
+                            "year": 2017,
+                        },
+                        {
+                            "ca": 432070,
+                            "margin": 427778,
+                            "ebitda": 290433,
+                            "loss": 8023406,
+                            "year": 2016,
+                        },
+                    ],
+                }
+            ],
+        )
+
+    def test_get_name_stats(self):
+        """
+        Ensure we can get all details of entreprise by name.
+        """
+        url = reverse("entreprise-name-stats", kwargs={"name": "Reinger Inc"})
+        response = self.client.get(url, format="None")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data,
+            [
+                {
+                    "name": "Reinger Inc",
+                    "sector": "Services",
+                    "siren": "135694027",
+                    "results": [
+                        {
+                            "year": "2016-2017",
+                            "ca": "+380.79%",
+                            "margin": "+16.26%",
+                            "ebitda": "-77.29%",
+                            "loss": "-89.30%",
+                        }
+                    ],
+                }
             ],
         )
